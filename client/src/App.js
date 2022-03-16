15625629c08f681d7206705036ef0d124bea5d73
FedexaZ <fedexaz3@gmail.com>
2022-03-16 11:55:03 -0300

    Agregado formulario de registro (sin controlar aun)

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Register from './Components/Register'
+import Register from './Components/Register/Register'
 
 function App() {
   return (
