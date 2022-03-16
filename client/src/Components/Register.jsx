b8b9703b2f3867408dce2f55e7d43f3079c7a88a
FedexaZ <fedexaz3@gmail.com>
2022-03-16 10:14:21 -0300

    Agregado componente register en la app

--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -1,15 +1,24 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { Container } from '@mui/material';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import StarIcon from '@mui/icons-material/Star';
 
 export default function InputAdornments() {
-  const [values, setValues] = React.useState({
+  const [values, setValues] = useState({
     amount: '',
     password: '',
     weight: '',
@@ -33,29 +42,94 @@ export default function InputAdornments() {
   };
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px' }}>
-      <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-        <OutlinedInput
-          id="outlined-adornment-password"
-          type={values.showPassword ? 'text' : 'password'}
-          value={values.password}
-          onChange={handleChange('password')}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {values.showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-          label="Password"
-        />
-      </FormControl>
-    </Box>
+    <Container>
+      <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around' }} >
+        <Box>
+          <h1 style={{ fontWeight: '300', fontSize: '60px' }}>
+            Register at <br />
+            <span style={{ fontWeight: '400', fontSize: '32px', position: 'relative', top: '-50px', left: '0px'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Eztinerary</span>
+          </h1>
+          <p style={{ position: 'relative', top: '-50px', left: '0px', fontSize: '18px' }}>
+            Disfrutarás de beneficios como:
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }} aria-label="contacts">
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <StarIcon color='success' />
+                  </ListItemIcon>
+                  <ListItemText primary="Tener una lista de itinerarios favoritos" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <StarIcon color='success' />
+                  </ListItemIcon>
+                  <ListItemText primary="Búsqueda más personalizada en base a tus preferencias" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <StarIcon color='success' />
+                  </ListItemIcon>
+                  <ListItemText primary="¡Entre muchos más!" />
+                </ListItemButton>
+              </ListItem>
+              
+            </List>
+            <Button variant='contained'>Conocer más beneficios</Button>
+          </p>
+        </Box>
+        <Box>
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'center', marginTop: '50px' }}>
+            <FormControl sx={{ m: 1, width: 'auto' }} variant="outlined">
+              <InputLabel htmlFor="name">Name</InputLabel>
+              <OutlinedInput
+                id="name"
+                type='text'
+                value={values.password}
+                onChange={handleChange('password')}
+                label="Name"
+              />
+            </FormControl>
+            <FormControl sx={{ m: 1, width: 'auto' }} variant="outlined">
+              <InputLabel htmlFor="surname">Surname</InputLabel>
+              <OutlinedInput
+                id="surname"
+                type='text'
+                value={values.password}
+                onChange={handleChange('password')}
+                label="Surname"
+              />
+            </FormControl>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+            <FormControl sx={{ m: 1, width: 'auto' }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={values.showPassword ? 'text' : 'password'}
+                value={values.password}
+                onChange={handleChange('password')}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
   );
 }
