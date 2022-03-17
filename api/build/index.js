98b0f2bf67846a02c1bc2f9ae41849157b265e03
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-03-17 17:14:36 -0600

    Added some libraries and fixed a error in index.ts

--- api/build/index.js
+++ api/build/index.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./src/app"));
-require("./src/db");
 app_1.default.listen(3001, () => {
     console.log('Server listening at 3001');
 });
