94b51d70507c37be7df0c18ddad5c56e38c7ef50
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-03-18 11:54:10 -0600

    Changes to support Google register and maybe login

--- api/build/src/routes/signin.route.js
+++ api/build/src/routes/signin.route.js
@@ -4,4 +4,7 @@ const express_1 = require("express");
 const signin_controller_1 = require("../controllers/signin.controller");
 const router = (0, express_1.Router)();
 router.post('/', signin_controller_1.signInController);
+router.get('/google', signin_controller_1.signInGoogleController);
+router.get('/google/failure', signin_controller_1.signInGoogleFailureController);
+router.get('/google/callback', signin_controller_1.signInGoogleCallBackController);
 exports.default = router;
