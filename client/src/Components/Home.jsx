12bc004c5f31260a929128e4aa3803b2f0512543
Jhon.Crow <juancho.cuervo@hotmail.com>
2022-03-18 18:03:11 -0500

    Create Activiy card & detail component

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import NavBar from './NavBar';
 import LoginForm from './LoginForm'
+import ActivityCard from './ActivityCard';
+import ActivityDetail from './ActivityDetail';
 
 
 export default function Home() {
     const [loginForm, setLoginForm] = useState(null);
+    const [detail, setDetail] = useState(null);
 
     return (
          <>
@@ -18,11 +21,15 @@ export default function Home() {
                     Hola Viajero, ¿A donde quieres ir?
                 </label>
             </div>
+            <ActivityCard
+            handleDetail={setDetail}/>
 
             </div>
-            
             {loginForm &&
             <LoginForm activity={loginForm} close={() => setLoginForm(null)} />}
+
+            {detail &&
+            <ActivityDetail activity={detail} close={() => setDetail(null)} />}
         </>
     );
 };
