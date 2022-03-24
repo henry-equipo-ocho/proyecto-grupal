324f12229b679e4be00eb1198073f3e77c664122
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-03-24 10:01:55 -0600

    Testing

---
+++ api/src/controllers/user.controller.ts
@@ -0,0 +1,6 @@
+import { Request, Response, RequestHandler } from 'express';
+import ServerResponse from '../interfaces/ServerResponse.interface';
+
+export const userPersonalInfoUpdate: RequestHandler = async (req: Request, res: Response) => {
+
+}
