276c31db8a4a240343416bca677fb07fd07d6807
FedexaZ <fedexaz3@gmail.com>
2022-03-25 11:36:52 -0300

    Agregando forma de traer datos del back

--- client/src/Components/UserDashboard/Favorites.jsx
+++ client/src/Components/UserDashboard/Favorites.jsx
@@ -3,29 +3,47 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import FavCard from './FavCard';
 
-const iti = [[1, 2, 3], [2], [3, 2, 2, 2, 2], [2, 1]]
+const iti = [[1, 2, 3], [2], [3, 2, 2, 2, 2], [2, 1]];
+
+const getFavorites = async () => {
+  try{
+    
+  }
+  catch(e){
+
+  }
+};
 
 export default function Favorites() {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Typography variant='h4' sx={{ marginTop: '15px', marginBottom: '10px' }}>Favorites</Typography>
-      <Box sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', flexDirection: 'column' }}>
       {
         iti.length 
         ? 
           iti.map((a, i) =>{
             return (
-              <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px', padding: '10px' }}>
-                  <Typography variant='h5' sx={{ margin: '10px' }}>Itinerary {i + 1} <Button variant="outlined" size="small">Remove itinerary</Button></Typography>
-                <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', border: '1px solid black', margin: '10px', padding: '10px' }}>
-                  {
-                    a.map((act, i) => <FavCard key={i} />)
-                  }
-                </Box>
-              </Box>
+              <Accordion sx={{ my: 1 }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                >
+                  <Typography>Itinerary {i + 1}</Typography>
+                </AccordionSummary>
+                <AccordionDetails sx={{ display: 'flex', flexDirection: 'column'}}>
+                  <Button variant="outlined" size="small" sx={{ display: 'inline-block' }}>Remove itinerary</Button>
+                  <Box sx={{ display: 'flex', flexDirection: 'row'}}>
+                  {a.map((act, i) => <FavCard key={i} />)}
+                  </Box>
+                </AccordionDetails>
+              </Accordion>
             )
           })
         :
