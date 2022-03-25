a30335342366d772be9a61fda50341d4dbdcd22b
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-03-25 09:38:56 -0600

    Readme update to modify user info

--- api/build/src/services/userUpdate.services.js
+++ api/build/src/services/userUpdate.services.js
@@ -32,12 +32,12 @@ const updatePersonalInfo = (req, id) => __awaiter(void 0, void 0, void 0, functi
 exports.updatePersonalInfo = updatePersonalInfo;
 const updatePassword = (req, id) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        User_models_1.default.findById(id, function (err, doc) {
+        User_models_1.default.findById(id, function (err, result) {
             return __awaiter(this, void 0, void 0, function* () {
                 if (err)
                     return false;
-                doc.password = req.body.password;
-                yield doc.save();
+                result.password = req.body.password;
+                yield result.save();
             });
         });
     }
