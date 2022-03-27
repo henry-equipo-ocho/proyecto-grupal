a5dbb0d1257d234af28337fbb64cbbe7027b48bc
FedexaZ <fedexaz3@gmail.com>
2022-03-27 16:02:06 -0300

    Fixed some bugs and added logic in favorites (add, delete activity and itinerary)

--- client/src/Components/UserDashboard/FavCard.jsx
+++ client/src/Components/UserDashboard/FavCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -7,32 +8,56 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-export default function ActivityCard({ name, pictures, shorTDescription, price }) {
-    return (
-        <Card sx={{ maxWidth: 245, margin: '10px', padding: '10px' }}>
-        <CardMedia
-          component="img"
-          height="140"
-          alt="Turismo"
-          image ={ pictures }
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography variant="body1" color="text.secondary">
-           {shorTDescription.slice(0, 50)}... <a href="#">read more</a>
-          </Typography>
-          <Typography variant="body1" color="text.secondary">
-           {price}
-          </Typography>
-        </CardContent>
-        <CardActions style={{justifyContent: 'center'}} >
-          <Button
+export default function ActivityCard({ name, pictures, shorTDescription, price, currency, link, loadFavs, actID, itID }) {
+  const remove = async (itineraryName, activityID) => {
+    try {
+      const token = JSON.parse(localStorage.getItem('token'));
+      await axios.delete('http://localhost:3001/favorites',
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          },
+          data: {
+            itineraryName,
+            activityID,
+            itineraryIndex: 0,
+          }
+        });
+      alert(`Activity "${name.slice(0, 15)}..." deleted from itinerary "${itineraryName}"`);
+    }
+    catch (e) {
+      console.log(e);
+    }
+    loadFavs();
+  }
+
+  return (
+    <Card sx={{ maxWidth: 245, margin: '10px', padding: '10px' }}>
+      <CardMedia
+        component="img"
+        height="140"
+        alt="Turismo"
+        image={pictures}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body1" color="text.secondary">
+          {shorTDescription.slice(0, 50)}... <a href={link} target='_blank' rel="noreferrer">read more</a>
+        </Typography>
+        <Typography variant="body1" color="text.secondary">
+          Price: {price} {currency}
+        </Typography>
+      </CardContent>
+      <CardActions style={{ justifyContent: 'center' }} >
+        <Button
           color="inherit"
           variant='outlined'
-          size="small">Remove from itinerary</Button>
-        </CardActions>
-      </Card>
-    );
+          size="small"
+          onClick={() => remove(itID, actID)}
+          >Remove from itinerary</Button>
+      </CardActions>
+    </Card>
+  );
 };
--- client/src/Components/UserDashboard/Favorites.jsx
+++ client/src/Components/UserDashboard/Favorites.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
 
@@ -10,60 +10,104 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+
+import { Link } from 'react-router-dom';
+
 import FavCard from './FavCard';
 
-let iti = [];
-
-const getFavorites = async () => {
-  try{
-    const token = JSON.parse(localStorage.getItem('token'));
-    const id = JSON.parse(localStorage.getItem('data')).id;
-    const response = await axios.get('http://localhost:3001/favorites/' + id, 
-    {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
-    iti = [...response.data.data];
-  }
-  catch(e){
-    console.log(e)
-  }
-};
-
 export default function Favorites() {
+  const [iti, setIti] = useState([]);
+
+  const remove = async (itineraryName) => {
+    try {
+      const token = JSON.parse(localStorage.getItem('token'));
+        await axios.delete('http://localhost:3001/favorites',
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          },
+          data:{
+            itineraryName,
+            itineraryIndex: 0,
+          }
+        });
+        alert(`Itinerary "${itineraryName}" deleted!`);
+      }  
+    catch (e) {
+      console.log(e);
+    }
+    getFavorites();
+  }
+  
+  const getFavorites = async () => {
+    try {
+      const token = JSON.parse(localStorage.getItem('token'));
+      const response = await axios.get('http://localhost:3001/favorites/',
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+      setIti([...response.data.data]);
+    }
+    catch (e) {
+      console.log(e)
+    }
+  };
   
   useEffect(() => {
     getFavorites();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Typography variant='h4' sx={{ marginTop: '15px', marginBottom: '10px' }}>Favorites</Typography>
       <Box sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', flexDirection: 'column' }}>
-      {
-        iti.length 
-        ? 
-          iti.map((a, i) =>{
-            return (
-              <Accordion key={i} sx={{ my: 1, width: '100vw' }}>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                >
-                  <Typography>Itinerary {i + 1}</Typography>
-                </AccordionSummary>
-                <AccordionDetails sx={{ display: 'flex', flexDirection: 'column'}}>
-                  <Button variant="outlined" size="small" sx={{ display: 'inline-block' }}>Remove itinerary</Button>
-                  <Box sx={{ display: 'flex', flexDirection: 'row'}}>
-                  {/* {a.map((act, i) => <FavCard key={i} />)} */}
-                  </Box>
-                </AccordionDetails>
-              </Accordion>
-            )
-          })
-        :
-        <Typography variant='h4' sx={{ marginTop: '45px', marginBottom: '10px' }}>No favorites</Typography>
-      }
+        {
+          iti.length
+            ?
+            iti.map((itinerary, i) => {
+              return (
+                <Accordion key={i} sx={{ my: 1, width: '100vw' }}>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                  >
+                    <Typography>{itinerary.name}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Button variant="outlined" size="small" sx={{ display: 'inline-block' }} onClick={() => remove(itinerary.name)}>Remove itinerary</Button>
+                    <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                      {
+                        itinerary.activities.length ? 
+                          itinerary.activities.map((act, i) => <FavCard 
+                          key={act._id} 
+                          shorTDescription={act.description} 
+                          link={act.booking} 
+                          name={act.name} 
+                          price={act.price_amount} 
+                          currency={act.price_currency} 
+                          actID={act.id} 
+                          itID={itinerary.name}
+                          pictures={act.picture}
+                          loadFavs={getFavorites}
+                          />)
+                        :
+                        <Alert severity="error" sx={{ width: '100%', my: 1 }}>
+                          <AlertTitle>Activities</AlertTitle>
+                          No activities to display — <strong><Link to='/home' style={{ textDecoration: 'none' }}><Button>Try add activities here!</Button></Link></strong>
+                        </Alert>
+                      }
+                    </Box>
+                  </AccordionDetails>
+                </Accordion>
+              )
+            })
+            :
+            <Typography variant='h4' sx={{ marginTop: '45px', marginBottom: '10px' }}>No favorites</Typography>
+        }
       </Box>
     </Box>
   )
