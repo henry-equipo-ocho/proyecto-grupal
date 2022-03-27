23b159db4564702258cff1c456dd45f9fde6b22e
Jhon.Crow <juancho.cuervo@hotmail.com>
2022-03-26 19:58:09 -0500

    Added new LandingPage

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import Register from './Components/Register/Register'
 import Slideshow from './Components/Landingpage/Slideshow'
 import Home from './Components/Home'
+import LandingPage from './Components/LandingPage';
 
 function App() {
   return (
     <div>
     <Routes>
-        <Route exact path='/' element={<Slideshow />} />
+        <Route exact path='/' element={<LandingPage />} />
        {/*  <Route exact path='/home' element={<NavBar />} /> */}
         <Route exact path='/home' element={<Home />} />
         <Route exact path='/register' element={<Register />} />
