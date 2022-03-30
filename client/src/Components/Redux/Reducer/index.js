37aa09f97abeb3628f05879c0c4138ec7b20946b
Jhon.Crow <juancho.cuervo@hotmail.com>
2022-03-30 10:47:30 -0500

    Added filter and ordered operative

--- client/src/Components/Redux/Reducer/index.js
+++ client/src/Components/Redux/Reducer/index.js
@@ -3,13 +3,19 @@ import {
     SET_ALL_ACTIVITIES,
     GET_ACTIVITIES,
     SET_LOADING,
-    ORDER_ACTIVITIES,
+    ORDER_ACTIVITIES_BY_CITY,
+    ORDER_ACTIVITIES_BY_PRICE,
+    GET_COUNTRIES,
+    GET_CITIES,
+
 } from '../Actions/actions_types';
 
 const initialState = {
     userName: 'viajero',
     allActivities: [],
     currentActivities: [],
+    countries: [],
+    cities: [],
     loading: true,
 };
 
@@ -40,11 +46,27 @@ export default function rootReducer(state = initialState, action) {
                 ...state,
                 loading: action.payload
             };
-        case ORDER_ACTIVITIES:
+        case ORDER_ACTIVITIES_BY_CITY:
             return {
                 ...state,
                 allActivities: action.payload,
                 currentActivities: action.payload,
+            };
+        case ORDER_ACTIVITIES_BY_PRICE:
+            return {
+                ...state,
+                allActivities: action.payload,
+                currentActivities: action.payload,
+            };
+        case GET_COUNTRIES:
+            return {
+                ...state,
+                countries: action.payload,
+            };
+        case GET_CITIES:
+            return {
+                ...state,
+                cities: action.payload,
             };
 
         default:
