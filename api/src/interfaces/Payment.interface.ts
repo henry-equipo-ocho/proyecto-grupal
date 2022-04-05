10e137b13921470774fb7ad7d4852ba56e953bdd
Ale <alejoegarcia@gmail.com>
2022-04-04 21:41:06 -0300

    adds Payment docs to README, refactors payment.services captureOrder, adds attributes to User interface and model, creates FrontFacingPayment interface

--- api/src/interfaces/Payment.interface.ts
+++ api/src/interfaces/Payment.interface.ts
@@ -2,11 +2,21 @@ export enum PaymentStatus {
     CREATED,
     COMPLETED
 }
+
+export interface FrontFacingPayment {
+    name: string,
+    email: string,
+    tier: number,
+    price: number,
+    buyDate: Date,
+    expireDate: Date
+}
 export default interface Payment {
     id: string,
     status: string,
     description: string,
     tier: number,
-    createdAt?: number,
-    updatedAt?: number
+    price: number,
+    createdAt?: Date,
+    updatedAt?: Date
 }
