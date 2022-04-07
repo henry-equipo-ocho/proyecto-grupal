29d0eca4757c6c4370ad15e68158a86de16f3980
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-04-07 00:12:28 -0500

    Updates for deploy

--- api/build/index.js
+++ api/build/index.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./src/app"));
-app_1.default.listen(3001, () => {
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+app_1.default.listen(process.env.PORT || 3001, () => {
     console.log('Server listening at 3001');
 });
--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,8 @@
 import server from './src/app';
+import dotenv from 'dotenv';
 
-server.listen(3001, () => {
+dotenv.config();
+
+server.listen(process.env.PORT || 3001, () => {
     console.log('Server listening at 3001')
 });
